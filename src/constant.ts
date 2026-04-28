25710eea8ef182b796ab8df3e3e55573d5f1a9a3
lonewolfyx <olddrivero.king@qq.com>
2026-04-28 10:54:59 +0800

    refactor(utils): move constants to dedicated module with config file path

--- src/constant.ts
+++ src/constant.ts
@@ -1,5 +1,5 @@
-import { dirname } from 'node:path'
-import * as process from 'node:process'
+import { dirname, resolve } from 'node:path'
+import process from 'node:process'
 import { fileURLToPath } from 'node:url'
 
 const __filename = fileURLToPath(import.meta.url)
@@ -7,3 +7,5 @@ const __filename = fileURLToPath(import.meta.url)
 export const ROOT_PATH = dirname(__filename)
 
 export const CLIENT_PATH = process.cwd()
+
+export const CONFIG_FILE = resolve(ROOT_PATH, 'ai.config.json')
